d8c45c6d21edfbec003869d81287a16ed82b1c67
Jonathan Samudio <jonathan2457@gmail.com>
2018-05-08 09:14:41 -0400

    Removed xibs from the sources and set version to 9.0

--- Example/Tests/Features/BackgroundOverlayView/BackgroundOverlayViewTests.swift
+++ Example/Tests/Features/BackgroundOverlayView/BackgroundOverlayViewTests.swift
@@ -17,7 +17,7 @@ class BackgroundOverlayViewTests: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        sut = BackgroundOverlayView.instanceFromNib()
+        sut = BackgroundOverlayView()
         
         fakeAlphaAnimator = FakeAlphaAnimator()
         sut.alphaAnimator = fakeAlphaAnimator
---
+++ Velar/Classes/Extensions/UIApplicationExtension.swift
@@ -0,0 +1,26 @@
+//
+//  UIApplicationExtension.swift
+//  Pods-Velar_Example
+//
+//  Created by Jonathan Samudio on 5/8/18.
+//
+
+import Foundation
+
+extension UIApplication {
+
+    /// The safe area insets of the application.
+    ///
+    /// - Returns: Edge insets of the application.
+    func safeAreaInsets() -> UIEdgeInsets {
+        guard let rootView = UIApplication.shared.keyWindow else {
+            return UIEdgeInsets.zero
+        }
+        if #available(iOS 11.0, *) {
+            return rootView.safeAreaInsets
+        } else {
+            return .zero
+        }
+    }
+
+}
