4f3ed5d846d2e7436ec0a336fef9413ede5c7dc6
Jonathan Samudio <js5504@nyu.edu>
2018-05-18 11:18:06 -0400

    Removed space.

--- Velar/Velar/AppDelegate.swift
+++ Velar/Velar/AppDelegate.swift
@@ -44,6 +44,4 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
